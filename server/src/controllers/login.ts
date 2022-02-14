a34e64a848fd12048cdaf0d11c90487216dab218
Richard Hyesung O <orichh@gmail.com>
2022-02-13 21:37:19 -0500

    Finish simple login

--- server/src/controllers/login.ts
+++ server/src/controllers/login.ts
@@ -1,4 +1,4 @@
-import { getPasswordForUser } from "../database/models/login";
+import { getPasswordForUser, getUserLists } from "../database/models/login";
 import Joi from "joi";
 
 const userSchema = Joi.object({
@@ -10,13 +10,26 @@ export const login = (req: any, res: any) => {
   // validate user submitted info
   const { error, value } = userSchema.validate(req.body);
 
+  // check if request body is valid
   if (error === undefined) {
     const { email, password } = value;
+
+    // get password for given email and check if it's correct
     getPasswordForUser(email)
       .then((response: any) => {
         const userPassword = response.rows[0].password;
+        const userId = response.rows[0].id;
+
+        // if password matches, send back user id and user's lists
         if (userPassword === password) {
-          res.sendStatus(200);
+          getUserLists(userId)
+            .then((response: any) => {
+              const payload = { userId: userId, data: response.rows };
+              res.send(payload);
+            })
+            .catch((error: any) => {
+              res.sendStatus(500);
+            });
         } else {
           res.sendStatus(401);
         }
--- server/src/database/models/login.ts
+++ server/src/database/models/login.ts
@@ -3,7 +3,7 @@ import { pool } from "../index";
 // check if email and password match
 export const getPasswordForUser = (email: string) => {
   const queryString = `
-    SELECT password
+    SELECT id, password
       FROM users
      WHERE email=$1
   `;
@@ -15,12 +15,18 @@ export const getPasswordForUser = (email: string) => {
   return pool.query(query);
 };
 
-// export const login = () => {
-//   const queryString = `;`;
-//   const query = {
-//     name: "login",
-//     text: queryString,
-//     values: [],
-//   };
-//   return pool.query(queryString);
-// };
+export const getUserLists = (id: number) => {
+  const queryString = `
+    SELECT u.id, m.name, u.first_name, u.last_name, ul.date_added, ul.rating
+    FROM users_lists as ul
+    JOIN movies as m ON m.id=ul.movie_id
+    JOIN users as u ON u.id=ul.user_id
+    WHERE u.id = $1
+  `;
+  const query = {
+    name: "get-user-lists",
+    text: queryString,
+    values: [id],
+  };
+  return pool.query(query);
+};
