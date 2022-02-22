813b83b3b2df281d2daab923ccea3e445521b1fb
Richard Hyesung O <orichh@gmail.com>
2022-02-21 20:07:01 -0500

    Clean up login

--- server/src/database/models/login.ts
+++ server/src/database/models/login.ts
@@ -15,18 +15,4 @@ export const getPasswordForUser = (email: string) => {
   return pool.query(query);
 };
 
-export const getUserLists = (id: number) => {
-  const queryString = `
-    SELECT m.name, ul.date_added, ul.rating
-    FROM users_lists as ul
-    JOIN movies as m ON m.id=ul.movie_id
-    JOIN users as u ON u.id=ul.user_id
-    WHERE u.id = $1
-  `;
-  const query = {
-    name: "get-user-lists",
-    text: queryString,
-    values: [id],
-  };
-  return pool.query(query);
-};
+
